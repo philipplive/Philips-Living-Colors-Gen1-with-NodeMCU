27da91cd8a476727b519884cd05ac5c696161113
unknown <philipplive88@gmail.com>
2020-10-25 21:29:54 +0100

    update

--- PhilipsLampLib.cpp
+++ PhilipsLampLib.cpp
@@ -11,132 +11,125 @@ PhilipsLampLib::PhilipsLampLib() {
   pinMode(SPI_CS, OUTPUT);
   digitalWrite(SPI_CS, HIGH);
 
-  delay(1500);
+  delay(1);
 
   // reset device (0x30)
   reset();
 
-  // reset device (0x30)
-  reset();
-
-  // write configuration register: FSCTRL1 – Frequency Synthesizer Control
+  //FSCTRL1 – Frequency Synthesizer Control
   sendCommand(0x0B, 0x09);
 
-  // write configuration register: FSCTRL0 – Frequency Synthesizer Control
+  //FSCTRL0 – Frequency Synthesizer Control
   sendCommand(0x0C, 0x00);
 
-  // write configuration register: FREQ2 – Frequency Control Word, High Byte
+  //FREQ2 – Frequency Control Word, High Byte
   sendCommand(0x0D, 0x5D);
 
-  // write configuration register: FREQ1 – Frequency Control Word, Middle Byte
+  //FREQ1 – Frequency Control Word, Middle Byte
   sendCommand(0x0E, 0x93);
 
-  // write configuration register: FREQ0 – Frequency Control Word, Low Byte
+  //FREQ0 – Frequency Control Word, Low Byte
   sendCommand(0x0F, 0xB1);
 
-  // write configuration register: MDMCFG4 – Modem Configuration
+  //MDMCFG4 – Modem Configuration
   sendCommand(0x10, 0x2D);
 
-  // write configuration register: MDMCFG3 – Modem Configuration
+  //MDMCFG3 – Modem Configuration
   sendCommand(0x11, 0x3B);
 
-  // write configuration register: MDMCFG2 – Modem Configuration
+  //MDMCFG2 – Modem Configuration
   sendCommand(0x12, 0x73);
 
-  // write configuration register: MDMCFG1 – Modem Configuration
+  //MDMCFG1 – Modem Configuration
   sendCommand(0x13, 0x22);
 
-  // write configuration register: MDMCFG0 – Modem Configuration
+  //MDMCFG0 – Modem Configuration
   sendCommand(0x14, 0xF8);
 
-  // write configuration register: CHANNR – Channel Number
+  //CHANNR – Channel Number
   sendCommand(0x0A, 0x03);
 
-  // write configuration register: DEVIATN – Modem Deviation Setting
+  //DEVIATN – Modem Deviation Setting
   sendCommand(0x15, 0x00);
 
-  // write configuration register: FREND1 – Front End RX Configuration
+  //FREND1 – Front End RX Configuration
   sendCommand(0x21, 0xB6);
 
-  // write configuration register: FREND0 – Front End TX configuration
+  //FREND0 – Front End TX configuration
   sendCommand(0x22, 0x10);
 
-  // write configuration register: MCSM0 – Main Radio Control State Machine
+  //MCSM0 – Main Radio Control State Machine
   // Configuration
   sendCommand(0x18, 0x18);
 
-  // write configuration register: FOCCFG – Frequency Offset Compensation
+  //FOCCFG – Frequency Offset Compensation
   // Configuration
   sendCommand(0x19, 0x1D);
 
-  // write configuration register: BSCFG – Bit Synchronization Configuration
+  //BSCFG – Bit Synchronization Configuration
   sendCommand(0x1A, 0x1C);
 
-  // write configuration register: AGCCTRL2 – AGC Control
+  //AGCCTRL2 – AGC Control
   sendCommand(0x1B, 0xC7);
 
-  // write configuration register: AGCCTRL1 – AGC Control
+  //AGCCTRL1 – AGC Control
   sendCommand(0x1C, 0x00);
 
-  // write configuration register: AGCCTRL0 – AGC Control
+  //AGCCTRL0 – AGC Control
   sendCommand(0x1D, 0xB2);
 
-  // write configuration register: FSCAL3 – Frequency Synthesizer Calibration
+  //FSCAL3 – Frequency Synthesizer Calibration
   sendCommand(0x23, 0xEA);
 
-  // write configuration register: FSCAL2 – Frequency Synthesizer Calibration
+  //FSCAL2 – Frequency Synthesizer Calibration
   sendCommand(0x24, 0x0A);
 
-  // write configuration register: FSCAL1 – Frequency Synthesizer Calibration
+  //FSCAL1 – Frequency Synthesizer Calibration
   sendCommand(0x25, 0x00);
 
-  // write configuration register: FSCAL0 – Frequency Synthesizer Calibration
+  //FSCAL0 – Frequency Synthesizer Calibration
   sendCommand(0x26, 0x11);
 
-  // write configuration register: TEST2 – Various Test Settings
+  //TEST2 – Various Test Settings
   sendCommand(0x2C, 0x88);
 
-  // write configuration register: TEST1 – Various Test Settings
+  //TEST1 – Various Test Settings
   sendCommand(0x2D, 0x31);
 
-  // write configuration register: TEST0 – Various Test Settings
+  //TEST0 – Various Test Settings
   sendCommand(0x2E, 0x0B);
 
-  // write configuration register: IOCFG2 – GDO2 Output Pin Configuration
+  //IOCFG2 – GDO2 Output Pin Configuration
   sendCommand(0x00, 0x06);
 
-  // write configuration register: IOCFG0 – GDO0 Output Pin Configuration
+  //IOCFG0 – GDO0 Output Pin Configuration
   sendCommand(0x01, 0x01);
 
-  // write configuration register: PKTCTRL1 – Packet Automation Control
+  //PKTCTRL1 – Packet Automation Control
   sendCommand(0x07, 0x04);
 
-  // write configuration register: PKTCTRL0 – Packet Automation Control
+  //PKTCTRL0 – Packet Automation Control
   sendCommand(0x08, 0x45);
 
-  // write configuration register: ADDR – Device Address
+  //ADDR – Device Address
   sendCommand(0x09, 0x00);
 
-  // write configuration register: PKTLEN – Packet Length
+  //PKTLEN – Packet Length
   sendCommand(0x06, 0xFF);
 
-  // write configuration register: FIFOTHR – RX FIFO and TX FIFO Thresholds
+  //FIFOTHR – RX FIFO and TX FIFO Thresholds
   sendCommand(0x03, 0x0D);
 
-  // write power setting to PATABLE memory using single access write. See table
-  // 31 on page 47 of datasheet original value is 0xA9
+  // PATABLE - Output Power Programming
   sendCommand(0x3E, 0xFF);
 
-  // SIDLE: exit RX/TX
+  // SIDLE
   sendStrobe(CC2500_CMD_SIDLE);
 
-  // SIDLE: exit RX/TX
-  sendStrobe(CC2500_CMD_SIDLE);
-
-  // SPWD: enter power down mode when CSn goes high
+  // SPWD -  enter power down mode when CSn goes high
   sendStrobe(0x39);
 
-  // SIDLE: exit RX/TX
+  // SIDLE
   sendStrobe(CC2500_CMD_SIDLE);
 }
 
@@ -149,24 +142,25 @@ void PhilipsLampLib::reset() {
 }
 
 void PhilipsLampLib::scanLamps() {
-  // SIDLE: exit RX/TX
+  unsigned char result = 0;
+
+  // SIDLE
   sendStrobe(CC2500_CMD_SIDLE);
 
-  // read RX FIFO until empty
-  unsigned char result = sendCommand(0xFB, 0x00);
+  // RX FIFO länge auslesen
+  result = sendCommand(0xFB, 0x00);
 
   for (int i = 0; i < result; ++i) {
+    // Einzelnes Byte RX FIFO auslesen
     sendCommand(0xBF, 0x00);
   }
 
-  unsigned long startTime = millis();
-
-  // try learning addresses for 5 seconds
-  while ((millis() - startTime) < 5000) {
+  // Einige Durchläufe lang versuchen
+ for (int i = 0; i < 30; ++i) {
     // SRX: enable RX
     sendStrobe(0x34);
 
-    // wait a bit
+    // Kurz warten, bis RX gefüllt wird (falls jemand sendet)
     delay(100);
 
     // Anzahl Bytes im RX (17)
